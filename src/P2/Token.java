d54a1a686fa583ca21d06abbd137e2183bcf9fc9
omaroskars <omar13@ru.is>
2015-02-11 17:43:45 +0000

    added Token class

---
+++ src/P2/Token.java
@@ -0,0 +1,15 @@
+package p2;
+
+enum TokenCode { 
+	ID, ASSIGN, SEMICOL, INT, 
+	PLUS, MINUS, MULT, LPAREN, 
+	RPAREN, PRINT, END, ERROR
+}
+
+public class Token {
+	
+	public String lexeme;
+	public TokenCode tCode;
+	
+	
+}
