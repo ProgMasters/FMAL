b8fc08a7848996f475dd615588748102f2bfdcb0
Snaebjorn Eyjolfsson <snabbie1@gmail.com>
2015-02-12 20:54:55 +0000

    changed parse to public

--- src/P2/Parser.java
+++ src/P2/Parser.java
@@ -22,20 +22,44 @@ public class Parser {
 	
 	// All other methods are private methods
 	
-	private void parse() {
+	public void parse() {
 		// TODO: Initiate the parse
 		Statements();
 	}
 	
 	private void Statements() {
 		token = lexer.nextToken();
-		if(token.tCode == TokenCode.END) {
-			
+		if (token.tCode == TokenCode.END) {
+			System.out.println("END");
+		} else {
+			Statement();
+			Statements();
 		}
 	}
 	
 	private void Statement() {
-		
+		token = lexer.nextToken();
+		if (token.tCode == TokenCode.ID) {
+			System.out.println("PUSH " + token.lexeme); // PUSH command for ID token
+			
+			token = lexer.nextToken();
+			
+			if (token.tCode == TokenCode.ASSIGN) {
+				token = lexer.nextToken();
+				Expr();
+			} else {
+				error();
+			}
+			
+		} else if (token.tCode == TokenCode.PRINT) {
+			token = lexer.nextToken();
+			
+			if (token.tCode == TokenCode.ID) {
+				System.out.println(")
+			}
+		} else {
+			error();
+		}
 	}
 	
 	private void Expr() {
@@ -51,6 +75,6 @@ public class Parser {
 	}
 	
 	private void error() {
-		
+		System.out.println("Syntax error!");
 	}
 }
